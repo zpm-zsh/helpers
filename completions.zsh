4e335dc8d87e07993835a700a58264666fe3b421
Grigorii Horos <horosgrisa@gmail.com>
2019-04-16 20:48:59 +0300

    Fix

--- completions.zsh
+++ completions.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-zstyle ':completion:*:processes' command 'NOCOLORS=1 ps -U $(whoami)|sed "/ps/d"'
+zstyle ':completion:*:processes' command 'NOCOLORS=1 ps -U $USER|sed "/ps/d"'
 zstyle ':completion:*:processes' insert-ids menu yes select
 zstyle ':completion:*:processes-names' command 'NOCOLORS=1 ps xho command|sed "s/://g"'
 zstyle ':completion:*:processes' sort false
--- zsh-helpers.plugin.zsh
+++ zsh-helpers.plugin.zsh
@@ -1,32 +1,12 @@
 #!/usr/bin/env zsh
 
-source ${${(%):-%x}:a:h}/completions.plugin.zsh
-source ${${(%):-%x}:a:h}/correct.plugin.zsh
-source ${${(%):-%x}:a:h}/history.plugin.zsh
-source ${${(%):-%x}:a:h}/keyboard.plugin.zsh
-source ${${(%):-%x}:a:h}/modules.plugin.zsh
-source ${${(%):-%x}:a:h}/cd.plugin.zsh
-source ${${(%):-%x}:a:h}/path-fix.plugin.zsh
+source ${${(%):-%x}:a:h}/functions.zsh
+source ${${(%):-%x}:a:h}/completions.zsh
+source ${${(%):-%x}:a:h}/correct.zsh
+source ${${(%):-%x}:a:h}/history.zsh
+source ${${(%):-%x}:a:h}/keyboard.zsh
+source ${${(%):-%x}:a:h}/modules.zsh
+source ${${(%):-%x}:a:h}/cd.zsh
+source ${${(%):-%x}:a:h}/path-fix.zsh
 
 
-
-is-recursive-exist(){
-  local r_dir="$PWD"
-
-  if [[ -d "$r_dir/$1" || -f "$r_dir/$1" ]]; then
-    return 0
-    echo "$r_dir/$1"
-  fi
-  while [[ "$r_dir" != "" ]]; do
-    r_dir=$(dirname "$r_dir")
-    if [[ "$r_dir" == '/' ]]; then
-      r_dir=""
-    fi
-    if [[ -d "$r_dir/$1" || -f "$r_dir/$1" ]]; then
-      return 0
-    fi
-  done
-
-  return -1
-
-}
