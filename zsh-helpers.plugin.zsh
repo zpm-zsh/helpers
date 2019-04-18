60769bf80304b9142bc0c70ab7ba61a67901009d
Grigorii Horos <horosgrisa@gmail.com>
2019-04-18 17:16:17 +0300

    Initial commit

--- zsh-helpers.plugin.zsh
+++ zsh-helpers.plugin.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 
-source {${(%):-%x}:a:h}/check-deps.zsh
+source ${${(%):-%x}:a:h}/check-deps.zsh
 source ${${(%):-%x}:a:h}/appendpath.zsh
 source ${${(%):-%x}:a:h}/is-recursive-exist.zsh
 
