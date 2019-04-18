1998f78578298509ee14f186536b0d09f099efed
Grigorii Horos <horosgrisa@gmail.com>
2019-04-18 17:08:49 +0300

    Move all to core-config

--- path-fix.zsh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env zsh
-
-appendpath ~/.bin
-appendpath ~/.local/bin
--- zsh-helpers.plugin.zsh
+++ zsh-helpers.plugin.zsh
@@ -1,12 +1,4 @@
 #!/usr/bin/env zsh
 
 source ${${(%):-%x}:a:h}/functions.zsh
-source ${${(%):-%x}:a:h}/completions.zsh
-source ${${(%):-%x}:a:h}/correct.zsh
-source ${${(%):-%x}:a:h}/history.zsh
-source ${${(%):-%x}:a:h}/keyboard.zsh
-source ${${(%):-%x}:a:h}/modules.zsh
-source ${${(%):-%x}:a:h}/cd.zsh
-source ${${(%):-%x}:a:h}/path-fix.zsh
-
 
