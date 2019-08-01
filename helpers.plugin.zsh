6dea6c8a45539f0c87f0d5d7186c30e9710bd687
Grigorii Horos <horosgrisa@gmail.com>
2019-08-01 10:34:45 +0300

    Add usefull functions

--- helpers.plugin.zsh
+++ helpers.plugin.zsh
@@ -3,3 +3,6 @@
 source ${${(%):-%x}:a:h}/check-if.zsh
 source ${${(%):-%x}:a:h}/paths.zsh
 source ${${(%):-%x}:a:h}/is-recursive-exist.zsh
+source ${${(%):-%x}:a:h}/is-true.plugin.zsh
+source ${${(%):-%x}:a:h}/is-callable.plugin.zsh
+
