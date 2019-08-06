9f3c389e747c7e1b6bd7324994008c0b5b0c59a2
Grigorii Horos <horosgrisa@gmail.com>
2019-08-06 14:31:34 +0300

    Fix fn

--- is-recursive-exist.plugin.zsh
+++ is-recursive-exist.plugin.zsh
@@ -4,19 +4,17 @@ is-recursive-exist(){
   local r_dir="$PWD"
   
   if [[ -d "$r_dir/$1" || -f "$r_dir/$1" ]]; then
-    return 0
     echo "$r_dir/$1"
+	return 0
   fi
-  while [[ "$r_dir" != "" ]]; do
-    r_dir=$(dirname "$r_dir")
-    if [[ "$r_dir" == '/' ]]; then
-      r_dir=""
-    fi
+  while [[ "$r_dir" != "/" ]]; do
+    r_dir="${r_dir:h}"
     if [[ -d "$r_dir/$1" || -f "$r_dir/$1" ]]; then
-      return 0
+    echo -n "$r_dir/$1"  
+	return 0
     fi
   done
-  
+ 
   return -1
   
 }
