4973ffbf404996cff344d4c1565d5660967d0bf5
Grigorii Horos <horosgrisa@gmail.com>
2019-11-03 22:08:12 +0200

    Fix writable

--- is-recursive-exist.plugin.zsh
+++ is-recursive-exist.plugin.zsh
@@ -3,7 +3,7 @@
 is-recursive-exist(){
   local r_dir="$PWD"
   
-  if [[ ! -w "$r_dir/$1" ]]; then
+  if [[ ! -w "$r_dir" ]]; then
     return -1
   fi
 
@@ -13,7 +13,7 @@ is-recursive-exist(){
 
   while [[ "$r_dir" != "/" ]]; do
     r_dir="${r_dir:h}"
-    if [[ ! -w "$r_dir/$1" ]]; then
+    if [[ ! -w "$r_dir" ]]; then
       return -1
     fi
    if [[ -e "$r_dir/$1" ]]; then
