8b7d1e0f376449d325a2187c0ceb525b78bf6536
Grigorii Horos <horosgrisa@gmail.com>
2019-11-08 18:10:33 +0200

    Fix $0

--- helpers.plugin.zsh
+++ helpers.plugin.zsh
@@ -1,17 +1,22 @@
 #!/usr/bin/env zsh
 
+# Standarized $0 handling, following:
+# https://github.com/zdharma/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+_DIRNAME="${0:h}"
+
 DEPENDENCES_ZSH+=( zpm-zsh/colors )
 
 if (( $+functions[zpm] )); then
   zpm zpm-zsh/colors
 fi
 
-source ${${(%):-%x}:a:h}/path.plugin.zsh
-source ${${(%):-%x}:a:h}/hyperlink.plugin.zsh
-source ${${(%):-%x}:a:h}/debug.plugin.zsh
+source "${_DIRNAME}/path.plugin.zsh"
+source "${_DIRNAME}/hyperlink.plugin.zsh"
+source "${_DIRNAME}/debug.plugin.zsh"
 
-source ${${(%):-%x}:a:h}/check-if.plugin.zsh
-source ${${(%):-%x}:a:h}/is-recursive-exist.plugin.zsh
-source ${${(%):-%x}:a:h}/is-true.plugin.zsh
-source ${${(%):-%x}:a:h}/is-callable.plugin.zsh
-source ${${(%):-%x}:a:h}/is-older.plugin.zsh
+source "${_DIRNAME}/check-if.plugin.zsh"
+source "${_DIRNAME}/is-recursive-exist.plugin.zsh"
+source "${_DIRNAME}/is-true.plugin.zsh"
+source "${_DIRNAME}/is-callable.plugin.zsh"
+source "${_DIRNAME}/is-older.plugin.zsh"
