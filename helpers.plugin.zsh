56e8fe1d87ddb67e7a72fa29c03e9e48636b8c11
Grigorii Horos <horosgrisa@gmail.com>
2020-03-13 23:36:05 +0200

    Optimizations

--- helpers.plugin.zsh
+++ helpers.plugin.zsh
@@ -141,20 +141,13 @@ alias is=check-if
 
 is-recursive-exist(){
   local r_dir="$PWD"
-  
-  if [[ ! -w "$r_dir" ]]; then
-    return -1
-  fi
-  
+    
   if [[ -e "$r_dir/$1" ]]; then
     return 0
   fi
   
   while [[ "$r_dir" != "/" ]]; do
     r_dir="${r_dir:h}"
-    if [[ ! -w "$r_dir" ]]; then
-      return -1
-    fi
     if [[ -e "$r_dir/$1" ]]; then
       return 0
     fi
