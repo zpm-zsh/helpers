8a0f428d92f7dd4fa5412fd432789b6c2eb39e95
Grigorii Horos <horosgrisa@gmail.com>
2020-03-24 23:03:54 +0200

    Optimize

--- helpers.plugin.zsh
+++ helpers.plugin.zsh
@@ -4,10 +4,6 @@
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-if (( $+functions[zpm] )); then
-  zpm zpm-zsh/colors
-fi
-
 if [[ $PMSPEC != *f* ]] {
   fpath+=( "${0:h}/functions" )
 }
