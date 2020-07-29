ae6896db74498dc6eaa50208b04f8b297923f317
Grigorii Horos <horosgrisa@gmail.com>
2020-07-29 16:34:55 +0300

    Fix plugin

--- helpers.plugin.zsh
+++ helpers.plugin.zsh
@@ -1,5 +1,21 @@
 #!/usr/bin/env zsh
 
-autoload -Uz path fpath p appendpath appendfpath prependpath prependfpath is-recursive-exist check-if debug \
-  is-callable is-true hyperlink hyperlink-pr hyperlink-file hyperlink-file-pr
+# Standarized ZSH polyfills, following:
+# https://github.com/zdharma/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+if [[ $PMSPEC != *f* ]] {
+  fpath+=( "${0:h}/functions" )
+}
+
+autoload -Uz                      \
+  path fpath p appendpath         \
+  appendfpath prependpath         \
+  prependfpath is-recursive-exist \
+  check-if debug                  \
+  is-callable is-true             \
+  hyperlink hyperlink-pr          \
+  hyperlink-file hyperlink-file-pr
+
 alias is=check-if
