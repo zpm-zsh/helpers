0c7d8b223c12d9d2715868ea12b372a3e2681be7
Grigorii Horos <horosgrisa@gmail.com>
2021-02-22 11:24:44 +0200

    Optimize

--- plugin-options.zsh
+++ plugin-options.zsh
@@ -3,5 +3,4 @@
 zpm_plugin_source=false
 zpm_plugin_bin_path=false
 zpm_plugin_functions_path=true
-zpm_plugin_autoload=
-zpm_plugin_autoload_all=true
+zpm_plugin_autoload=appendfpath:appendpath:check-if:debug:fpath:hyperlink:hyperlink-file:hyperlink-file-pr:hyperlink-pr:is-callable:is-recursive-exist:is-true:p:path:prependfpath:prependpath
